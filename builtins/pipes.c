c360dc86a8e54f2e5812e72d909727c6e117d8c8
zohirben <zohirbenaissa00@gmail.com>
2023-10-05 06:26:25 +0100

    gathered builtins into general execution function

--- builtins/pipes.c
+++ builtins/pipes.c
@@ -68,7 +68,7 @@ void execute_pipelines(t_node **command_node, char **env, t_shell *g_struct)
 			close(pipes[0]);
 			close(pipes[1]);
 			redirections(current_node->redirect, g_struct);
-			execute_commands_pipes(current_node->args[0], current_node->args, env, g_struct);
+			general_execution(g_struct, args, 0);
 		}
 		else
 		{
